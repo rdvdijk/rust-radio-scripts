80d778f64abdd540b9b23c3c6660bed754622943
Roel van Dijk <roel@rustradio.org>
2020-11-21 13:09:52 +0100

    Use LAME to set id3v2 tags, and add cover art.

--- flac-to-mp3.sh
+++ flac-to-mp3.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
 # This script converts FLAC files to MP3 files.
+# The FLAC audio, tags and cover art are converted.
 #
-# Requires: flac, lame and id3.
+# Requires: flac and lame.
 #
 
 checkInstalled() { if ! [ -x "$(command -v $1)" ]; then echo "'$1' could not be found"; exit 1; fi }
 
 checkInstalled flac
 checkInstalled lame
-checkInstalled id3
 
 getTag() {
   local file=$1
@@ -18,9 +18,10 @@ getTag() {
 }
 
 for flacfile in *.flac; do
-  echo "$flacfile"
-
+  ARTWORK=/tmp/flac2mp3.artwork
   MP3FILE="mp3/${flacfile%.flac}.mp3"
+
+  echo "Converting: $flacfile --> $MP3FILE"
 
   ARIST=$(getTag "$flacfile" ARTIST)
   TITLE=$(getTag "$flacfile" TITLE)
@@ -29,7 +30,26 @@ for flacfile in *.flac; do
   TRACKNUMBER=$(getTag "$flacfile" TRACKNUMBER)
   DATE=$(getTag "$flacfile" DATE)
 
+  INCLUDEART=""
+  metaflac --export-picture-to=$ARTWORK "$flacfile"
+  if [[ -f $ARTWORK ]]; then
+    INCLUDEART=true
+  fi
+
   mkdir -p mp3
-  flac -c -d "$flacfile" | lame -m j -q 0 --vbr-new -V 0 -s 44.1 - "$MP3FILE"
-  id3 -t "$TITLE" -T "${TRACKNUMBER:-0}" -a "$ARTIST" -A "$ALBUM" -y "$DATE" -g "${GENRE:-12}" "$MP3FILE"
+  flac -s -c -d "$flacfile" | \
+    lame --silent -m j -q 0 --vbr-new -V 0 -s 44.1 \
+    --add-id3v2 \
+    --tt "$TITLE" \
+    --ta "$ARTIST" \
+    --tl "$ALBUM" \
+    --ty "${DATE:0:4}" \
+    --tg "${GENRE:-12}" \
+    ${INCLUDEART:+ --ti $ARTWORK} \
+    --tn "${TRACKNUMBER:-0}" \
+    - "$MP3FILE"
+
+  if [[ -f $ARTWORK ]]; then
+    rm $ARTWORK
+  fi
 done
