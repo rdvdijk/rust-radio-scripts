e0d1d89a48cbe809c707030b016f4a16de2b342d
Roel van Dijk <roel@rustradio.org>
2020-12-24 17:20:24 +0100

    Initial FLAC tag printing script.

---
+++ flac-print-tags.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#
+# Get a single tag value.
+#
+tag_value () {
+  local file=$1
+  local tag=$2
+  echo $(metaflac --show-tag=$tag $file | cut -f2 -d"=")
+}
+export -f tag_value
+
+#
+# Print tags for a FLAC file.
+#
+print_tags () {
+  local file=$1
+
+  local tracknumber=$(tag_value $file TRACKNUMBER)
+  local title=$(tag_value $file TITLE)
+  local artist=$(tag_value $file ARTIST)
+  local date=$(tag_value $file DATE)
+  local album=$(tag_value $file ALBUM)
+
+  printf "%s. %-40s| %s | %s | %s\n" "$tracknumber" "$title" "$artist" "$date" "$album"
+}
+export -f print_tags
+
+find . -maxdepth 1 -name "*.flac" -print0 |
+  sort -z |
+  xargs -0 bash -c 'for f; do print_tags "$f"; done' _
+
